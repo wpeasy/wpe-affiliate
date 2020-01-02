1396d9c412d4a30d004257cc0d6a8263753f477b
Alan Blair <alan.blair@plana.com.au>
2020-01-02 14:26:25 +1000

    Working on display of Banner Images

--- wpe-affiliate.php
+++ wpe-affiliate.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPEasy Affiliate
  * Plugin URI:
  * Description: UI for Affiliates to generate HTML links and Shortcodes for WPEasy Adverts
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Alan Blair
  * Author URI:
  * Text Domain: wpeasy
