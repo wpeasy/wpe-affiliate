2c58f8d30d4cfef44e46990449da4c2c7801a189
alan.blair <KMhVDwtt6TM30bc0zBFV>
2020-06-22 15:06:58 +1000

    Disabled Updates

--- wpe-affiliate.php
+++ wpe-affiliate.php
@@ -3,22 +3,23 @@
  * Plugin Name: WPEasy Affiliate
  * Plugin URI:
  * Description: UI for Affiliates to generate HTML links and Shortcodes for WPEasy Adverts
- * Version: 1.0.10
+ * Version: 1.0.11
  * Author: Alan Blair
  * Author URI:
  * Text Domain: wpeasy
  */
 
 namespace WPEasyAffiliatePlugin;
-use WPEasyLibrary\WordPress\UpdateFromGithubController;
 use WPEasyLibrary\WordPress\WPEasyApplication;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+/*
 if ( is_admin() ) {
     new UpdateFromGithubController( __FILE__, 'wpeasy', "wpe-affiliate" );
 
 }
+*/
 $config = require __DIR__ . '/application.config.php';
 WPEasyApplication::init($config);
 WPEasyApplication::registerLoadedPlugin($config);
